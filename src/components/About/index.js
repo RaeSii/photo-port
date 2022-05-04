f0e778e3de46a3184983b77a6822804d33645f08
raesii <leisolo@gmail.com>
2022-05-04 16:21:19 -0700

    added lyza files

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import coverImage from "../../../assets/cover/"
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I</h1>
+        </section>
+
+    );
+}
+
+export default About;
