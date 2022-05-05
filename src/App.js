b2e81d486a7c8e0d306f433cfd76a0b2de0ee607
raesii <leisolo@gmail.com>
2022-05-04 17:53:04 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import About from './components/About';
 import coverImage from "./assets/cover/cover-image.jpg";
+import About from "../src/";
+import './App.css';
+
 
 function App() {
 
   return (
-    <div>
-<section className="my-5">
-  <h1 id="about">Who am I?</h1>
-  <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-</section>
-    </div>
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+    </section>
   );
 }
 
